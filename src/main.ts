11f070a835e28a9776e091ef26c86a6a220de3a4
8934173 <8934173@qq.com>
2021-12-19 20:31:51 +0800

    引入dataV

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+// @ts-ignore
+import dataV from '@jiaminghi/data-view';
 import './element-variables.scss';
-
 Vue.config.productionTip = false;
+
+Vue.use(dataV);
 
 new Vue({
   router,
